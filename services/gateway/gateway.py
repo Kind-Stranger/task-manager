4fddc786496c51cbf1e083abaff3b14d6469dfc8
Kind Stranger <kindstranger0@gmail.com>
2026-01-17 14:57:45 +0000

    Added session handler for retries

--- services/gateway/gateway.py
+++ services/gateway/gateway.py
@@ -2,8 +2,19 @@ import os
 import requests
 
 from flask import Flask, request, jsonify
+from requests.adapters import HTTPAdapter
+from urllib3 import Retry
 from common.healthcheck import register_health
 
+session = requests.Session()
+retries = Retry(
+    total=3,              # number of retries
+    backoff_factor=0.5,   # exponential backoff factor
+    status_forcelist=[500, 502, 503, 504]
+)
+adapter = HTTPAdapter(max_retries=retries)
+session.mount("http://", adapter)
+session.mount("https://", adapter)
 
 app = Flask(__name__)
 register_health(app)
@@ -16,18 +27,18 @@ USER_ENDPOINT = os.getenv("USER_ENDPOINT")
 
 @app.get("/all")
 def get_everything():
-    tasks = requests.get(f"{TASK_URL}{TASK_ENDPOINT}").json()
-    users = requests.get(f"{USER_URL}{USER_ENDPOINT}").json()
+    tasks = session.get(f"{TASK_URL}{TASK_ENDPOINT}").json()
+    users = session.get(f"{USER_URL}{USER_ENDPOINT}").json()
     return jsonify({"tasks": tasks, "users": users})
 
 
 @app.post("/new-task")
 def new_task():
     data = request.json
-    return requests.post(f"{TASK_URL}{TASK_ENDPOINT}", json=data).json()
+    return session.post(f"{TASK_URL}{TASK_ENDPOINT}", json=data).json()
 
 
 @app.post("/new-user")
 def new_user():
     data = request.json
-    return requests.post(f"{USER_URL}{USER_ENDPOINT}", json=data).json()
+    return session.post(f"{USER_URL}{USER_ENDPOINT}", json=data).json()
